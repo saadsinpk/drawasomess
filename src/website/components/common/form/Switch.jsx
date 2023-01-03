1e01b1e1e9af77e837bc86be3459b4d6a0aa11e1
muzammaslam <121795260+muzammaslam@users.noreply.github.com>
2023-01-03 14:56:13 +0500

    muzammilaslam

--- src/website/components/common/form/Switch.jsx
+++ src/website/components/common/form/Switch.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 function Switch() {
-    const [enabled, setEnabled] = useState(localStorage.getItem("switch"));
+    const [enabled, setEnabled] = useState  (( eval( localStorage.getItem("switch"))));
 
     const settingSwitch = (e) => {
         setEnabled(!enabled);
@@ -11,7 +11,7 @@ function Switch() {
             body.classList.add("active")
         }
         else {
-            localStorage.setItem('theme', "light")
+            localStorage.setItem('theme', "ligth")
             localStorage.setItem('switch', false);
             body.classList.remove("active")
         }
