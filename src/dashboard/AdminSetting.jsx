38f5d2e116f38770097e93e239ddaacf8eb5409a
muzammaslam <121795260+muzammaslam@users.noreply.github.com>
2023-01-05 17:57:30 +0500

    muzammilaslam

--- src/dashboard/AdminSetting.jsx
+++ src/dashboard/AdminSetting.jsx
@@ -1,13 +1,12 @@
-import React,{useState,useEffect,useRef} from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import config from "../services/config.json";
 import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
-import {getTokenSession,getidSession} from "./utils/common";
+import { getTokenSession } from "./utils/common";
 import { toast } from "react-toastify";
-import Loader from './components/common/Loader';
-
+import Loader from "./components/common/Loader";
 
 function AdminSetting() {
   const [userdata, setUserdata] = useState({
@@ -18,29 +17,50 @@ function AdminSetting() {
     instagram_link: "",
     twitter_link: "",
     tiktok_link: "",
-    snapchat_link: ""
+    snapchat_link: "",
   });
-  const [submitbutton, setSubmitbutton] = useState(false)
+  const [submitbutton, setSubmitbutton] = useState(false);
   const isComponentMounted = useRef(true);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    if (isComponentMounted.current) { 
-      getData();
+    if (isComponentMounted.current) {
+      putData();
     }
     return () => {
       isComponentMounted.current = false;
       setLoading(true);
-    }
+    };
   }, []);
-  const getData = async () => {
+
+  const putData = async () => {
     axios.defaults.headers = {
       "Content-Type": "application/json",
-      "Authorization":`Bearer ${getTokenSession()}`,
+      Authorization: `Bearer ${getTokenSession()}`,
     };
-    axios.get(`${config.apiEndPoint}profile/1`,)
-       .then ((response) => {
-         setLoading(false);
-         setUserdata({
+
+    axios
+      .get(`${config.apiEndPoint}adminInfo`)
+      .then((response) => {
+        let a = response.data.userId;
+        getData(a);
+      })
+      .catch((error) => {
+        setLoading(true);
+        if (error.response.status === 401)
+          toast.error(error.response.data.message);
+        else toast.error("Something went wrong. Please try again later.");
+      });
+  };
+
+  const getData = async (a) => {
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getTokenSession()}`,
+    };
+    axios
+      .get(`${config.apiEndPoint}profile/${a}`)
+      .then((response) => {
+        setUserdata({
           primary_email: response.data.data.email,
           current_password: response.data.data.password,
           instagram_link: response.data.data.instagram_link,
@@ -48,148 +68,131 @@ function AdminSetting() {
           tiktok_link: response.data.data.tiktok_link,
           snapchat_link: response.data.data.snapchat_link == null && "",
           new_password: "",
-          confirm: ""
-        })
-       
+          confirm: "",
+        });
+        setLoading(false);
       })
       .catch((error) => {
         setLoading(true);
         if (error.response.status === 401)
-        toast.error(error.response.data.message);
+          toast.error(error.response.data.message);
         else toast.error("Something went wrong. Please try again later.");
       });
-  }
-    const validationSchema = yup.object({
-      primary_email: yup.string().email("Invaild Email").required("Must Required"),
-        new_password: yup.string(),
-        confirm: yup.string().oneOf([yup.ref('new_password'), null], 'Passwords must match')
+  };
+  const validationSchema = yup.object({
+    primary_email: yup
+      .string()
+      .email("Invaild Email")
+      .required("Must Required"),
+    new_password: yup.string(),
+    confirm: yup
+      .string()
+      .oneOf([yup.ref("new_password"), null], "Passwords must match"),
+  });
+
+  const onSubmit = async (values) => {
+    setSubmitbutton(true);
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getTokenSession()}`,
+    };
+    axios
+      .put(`${config.apiEndPoint}update/1`, values)
+      .then((response) => {
+        setSubmitbutton(false);
+        setLoading(false);
+        toast.success(response.data.message);
+      })
+      .catch((error) => {
+        setSubmitbutton(false);
+        setLoading(false);
+        if (error.response.status === 401) {
+          toast.error(error.response.data.errorMessage);
+        } else toast.error("Something went wrong. Please try again later.");
       });
-
-
-      const  onSubmit  = async (values) => {
-        setSubmitbutton(true)
-        axios.defaults.headers = {
-          "Content-Type": "application/json",
-          "Authorization":`Bearer ${getTokenSession()}`,
-        };
-        axios.put(`${config.apiEndPoint}update/1`,
-           values)
-          .then((response) => {
-            setSubmitbutton(false)
-            setLoading(false);
-            toast.success(response.data.message);
-          })
-          .catch((error) => {
-            setSubmitbutton(false)
-            setLoading(false);
-            if (error.response.status === 401)
-            {
-              toast.error(error.response.data.errorMessage);
-
-            }
-            else toast.error("Something went wrong. Please try again later.");
-          });
-      }
-      if (loading) return <Loader />;
+  };
+  if (loading) return <Loader />;
   return (
-    <div className='my-5 p-5'>
-   <div className='adminForm flex'>
-    <div className="adminFormleft p-4">
-    <div className="adminFormleftBox">Primary E-Mail</div>
-    <div className="adminFormleftBox">Current Password</div>
-    <div className="adminFormleftBox">New Password</div>
-    <div className="adminFormleftBox">Confirm</div>
-    <div className="adminFormleftBox">Instagram Link</div>
-    <div className="adminFormleftBox"> Twitter Link</div>
-    <div className="adminFormleftBox">TikTok Link</div>
-    <div className="adminFormleftBox">Snapchat Link</div>
-    </div>
-    <div className="adminFormright">
-    <Formik
-          initialValues={userdata}
-          onSubmit={onSubmit}
-          validateOnChange
-          validationSchema={validationSchema}
-        >
-           {({ isSubmitting }) => {
-            return (
+    <div className="my-5 p-5">
+      <div className="adminForm flex">
+        <div className="adminFormleft p-4">
+          <div className="adminFormleftBox">Primary E-Mail</div>
+          <div className="adminFormleftBox">Current Password</div>
+          <div className="adminFormleftBox">New Password</div>
+          <div className="adminFormleftBox">Confirm</div>
+          <div className="adminFormleftBox">Instagram Link</div>
+          <div className="adminFormleftBox"> Twitter Link</div>
+          <div className="adminFormleftBox">TikTok Link</div>
+          <div className="adminFormleftBox">Snapchat Link</div>
+        </div>
+        <div className="adminFormright">
+          <Formik
+            initialValues={userdata}
+            onSubmit={onSubmit}
+            validateOnChange
+            validationSchema={validationSchema}
+          >
+            {({ isSubmitting }) => {
+              return (
                 <Form>
-                      <div className="inputBox">
-                  <Field
-                      type="text"
-                      name="primary_email"
-                    
-                    />
-                      <ErrorMessage name="primary_email">
-                    {(msg) => (
-                      <div style={{ color: "red", whiteSpace: "nowrap" }}>
-                        {msg}
-                      </div>
-                    )}
-                  </ErrorMessage>
+                  <div className="inputBox">
+                    <Field type="text" name="primary_email" />
+                    <ErrorMessage name="primary_email">
+                      {(msg) => (
+                        <div style={{ color: "red", whiteSpace: "nowrap" }}>
+                          {msg}
+                        </div>
+                      )}
+                    </ErrorMessage>
                   </div>
                   <div className="inputBox">
-                  <Field
-                      type="password"
-                      name="current_password"
-                    />
-                    </div>
+                    <Field type="password" name="current_password" />
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="password"
-                      name="new_password"
-                    />
-                    </div>
+                    <Field type="password" name="new_password" />
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="password"
-                      name="confirm"
-                    />
-                       <ErrorMessage name="confirm">
-                    {(msg) => (
-                      <div style={{ color: "red", whiteSpace: "nowrap" }}>
-                        {msg}
-                      </div>
-                    )}
-                  </ErrorMessage>
-                    </div>
+                    <Field type="password" name="confirm" />
+                    <ErrorMessage name="confirm">
+                      {(msg) => (
+                        <div style={{ color: "red", whiteSpace: "nowrap" }}>
+                          {msg}
+                        </div>
+                      )}
+                    </ErrorMessage>
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="text"
-                      name="instagram_link"
-                    />
-                    </div>
+                    <Field type="text" name="instagram_link" />
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="text"
-                      name="twitter_link"
-                    
-                    />
-                    </div>
+                    <Field type="text" name="twitter_link" />
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="text"
-                      name="tiktok_link"
-                    
-                    />
-                    </div>
+                    <Field type="text" name="tiktok_link" />
+                  </div>
                   <div className="inputBox">
-                  <Field
-                      type="text"
-                      name="snapchat_link"
-                    
-                    />
-                    </div>
-                  <div className='px-10'>  <button  type="submit"
-                    name="submit"  disabled={submitbutton} className='mt-4 btn btn-primary'> {submitbutton ? "login..." : "login"}</button></div>
-                    </Form>
-           );
-          }}
-        </Formik>
+                    <Field type="text" name="snapchat_link" />
+                  </div>
+                  <div className="px-10">
+                    {" "}
+                    <button
+                      type="submit"
+                      name="submit"
+                      disabled={submitbutton}
+                      className="mt-4 btn btn-primary"
+                    >
+                      {" "}
+                      {submitbutton ? "login..." : "login"}
+                    </button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
+      </div>
     </div>
-   </div>
-   </div>
-  )
+  );
 }
 
-export default AdminSetting
+export default AdminSetting;
